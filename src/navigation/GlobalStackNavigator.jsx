9fedf195563b660a95d70529bf811ededf0a9774
srikanth41684 <srikanthusnagiri41684@gmail.com>
2025-02-22 15:26:06 +0530

    header created

--- src/navigation/GlobalStackNavigator.jsx
+++ src/navigation/GlobalStackNavigator.jsx
@@ -2,6 +2,7 @@ import {View, Text} from 'react-native';
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import BottomTabNavigator from './BottomTabNavigator';
+import CreateTimerScreen from '../screens/CreateTimerScreen';
 
 const GlobalStackNavigator = () => {
   const GlobalStack = createNativeStackNavigator();
@@ -10,6 +11,11 @@ const GlobalStackNavigator = () => {
       <GlobalStack.Screen
         name="bottomTabNav"
         component={BottomTabNavigator}
+        options={{headerShown: false}}
+      />
+      <GlobalStack.Screen
+        name="createTimer"
+        component={CreateTimerScreen}
         options={{headerShown: false}}
       />
     </GlobalStack.Navigator>
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
-import {FlatList, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import React, {useEffect, useState} from 'react';
+import {
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
+import {Header} from '@react-navigation/elements';
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
   const data = [
     {
       id: 1,
@@ -28,6 +38,15 @@ const HomeScreen = () => {
       category: 'Break',
     },
   ];
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setProgress(prevProgress => (prevProgress < 100 ? prevProgress + 10 : 0));
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
   return (
     <SafeAreaView
       style={{
@@ -38,6 +57,20 @@ const HomeScreen = () => {
           flex: 1,
           backgroundColor: '#EFF1FE',
         }}>
+        <Header
+          headerLeft={() => (
+            <View
+              style={{
+                width: 40,
+                height: 40,
+                backgroundColor: 'lightgray',
+                borderRadius: 50,
+                marginLeft: 20,
+                marginRight: 10,
+              }}></View>
+          )}
+          title="Wellcome"
+        />
         <FlatList
           data={data}
           renderItem={({item}) => {
@@ -52,6 +85,32 @@ const HomeScreen = () => {
             );
           }}
         />
+
+        <TouchableWithoutFeedback
+          onPress={() => {
+            navigation.navigate('createTimer');
+          }}>
+          <View
+            style={{
+              width: 60,
+              height: 60,
+              backgroundColor: '#3D4AEE',
+              borderRadius: 50,
+              alignItems: 'center',
+              justifyContent: 'center',
+              alignSelf: 'flex-end',
+            }}>
+            <Text
+              style={{
+                fontSize: 18,
+                lineHeight: 25,
+                fontWeight: 'bold',
+                color: '#ffffff',
+              }}>
+              +
+            </Text>
+          </View>
+        </TouchableWithoutFeedback>
       </View>
     </SafeAreaView>
   );
